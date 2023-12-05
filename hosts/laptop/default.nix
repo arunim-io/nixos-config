c505369237fcf5b0d9f5734a89b1449e247d203d
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2023-12-06 02:11:51 +0600

    remove kde plasma

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -6,19 +6,5 @@
   ];
 
   networking.hostName = "hp-elitebook";
-
-  services.xserver.enable = true;
-
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "arunim";
-
   system.stateVersion = "23.11";
 }
